43674f46ecfc249007427bd1f0dcf6c5ad16ea48
Admin Happ <76879792+admin-happ@users.noreply.github.com>
2021-02-10 12:17:36 +0200

    Initial commit

---
+++ .commitlintrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  // "extends": ["@happ/commitlint-config"]
+}
---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+	// "overrides": [
+	//   {
+	//     "files": ["*.js", "*.ts"],
+	//     "extends": ["@happ/eslint-config"]
+	//   },
+	//   {
+	//     "files": ["*.ts"],
+	//     "extends": ["@happ/eslint-config-typescript"]
+	//   },
+	//   {
+	//     "files": ["*.spec.js", "*.spec.ts"],
+	//     "extends": ["@happ/eslint-config-jest"]
+	//   },
+	//   {
+	//     "files": ["ANGULAR_DIRECTORY/*.ts"],
+	//     "extends": ["@happ/eslint-config-angular"]
+	//   },
+	//   {
+	//     "files": ["ANGULAR_DIRECTORY/*.html"],
+	//     "extends": ["@happ/eslint-config-angular-template"]
+	//   },
+	//   {
+	//     "files": ["*.json"],
+	//     "extends": ["@happ/eslint-config-json"]
+	//   },
+	//   {
+	//     "files": ["*.md"],
+	//     "extends": ["@happ/eslint-config-markdown"]
+	//   }
+	// ]
+}
